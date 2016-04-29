b2542cbc0c412e0741b9c5af2b3ad7d906ba9c83
Dan Green <dangreenisrael@gmail.com>
2016-04-30 09:47:51 +1200

    Working with ACF theme includes

--- functions.php
+++ functions.php
@@ -1,57 +1,7 @@
 <?php
 require_once get_template_directory() . '/required-plugin-installer/install-plugins.php';
 
-if( function_exists('acf_add_options_page') ) {
-	acf_add_options_page(array(
-		'page_title' 	=> 'Home Page',
-		'menu_slug' 	=> 'home-page-options',
-		'icon_url' 		=> 'dashicons-admin-home',
-		'redirect'		=> true,
-		'position'		=> "3.01"
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Hero',
-		'menu_title' 	=> 'Hero',
-		'parent_slug'	=> 'home-page-options',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Services',
-		'menu_title' 	=> 'Services',
-		'parent_slug'	=> 'home-page-options',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Portfolio',
-		'menu_title' 	=> 'Portfolio',
-		'parent_slug'	=> 'home-page-options',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Timeline',
-		'menu_title' 	=> 'Timeline',
-		'parent_slug'	=> 'home-page-options',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Team',
-		'menu_title' 	=> 'Team',
-		'parent_slug'	=> 'home-page-options',
-	));
-	acf_add_options_sub_page(array(
-		'page_title' 	=> 'Contact',
-		'menu_title' 	=> 'Contact',
-		'parent_slug'	=> 'home-page-options',
-	));
 
-
-	acf_add_options_page(array(
-		'page_title' 	=> 'Site Settings',
-		'menu_slug' 	=> 'site-general-settings',
-		'capability'	=> 'edit_posts',
-		'icon_url'		=> 'dashicons-admin-customizer',
-		'redirect'		=> false,
-		'position'		=> "3.02"
-	));
-
-
-}
 
 /* functions.php */
 add_filter( 'timber_context', 'agency_timber_context'  );
@@ -165,39 +115,81 @@ add_action( 'wp_enqueue_scripts', 'agency_load_jquery' );
  * */
 
 //// 1. customize ACF path
-//add_filter('acf/settings/path', 'my_acf_settings_path');
+add_filter('acf/settings/path', 'my_acf_settings_path');
 //
-//function my_acf_settings_path( $path ) {
-//	// update path
-//	$path = get_stylesheet_directory() . '/acf/';
-//	// return
-//	return $path;
-//}
-////
-////
-////// 2. customize ACF dir
-//add_filter('acf/settings/dir', 'my_acf_settings_dir');
-//
-//function my_acf_settings_dir( $dir ) {
-//	// update path
-//	$dir = get_stylesheet_directory_uri() . '/acf/';
-//	// return
-//	return $dir;
-//}
+function my_acf_settings_path( $path ) {
+	// update path
+	$path = get_stylesheet_directory() . '/acf/advanced-custom-fields-pro/';
+	// return
+	return $path;
+}
+
+
+// 2. customize ACF dir
+add_filter('acf/settings/dir', 'my_acf_settings_dir');
+
+function my_acf_settings_dir( $dir ) {
+	// update path
+	$dir = get_stylesheet_directory_uri() . '/acf/advanced-custom-fields-pro/';
+	// return
+	return $dir;
+}
 
 
 // 3. Hide ACF field group menu item
-//add_filter('acf/settings/show_admin', '__return_false');
+add_filter('acf/settings/show_admin', '__return_false');
 
 
 // 4. Include ACF
-//include_once( get_stylesheet_directory() . '/acf/advanced-custom-fields/acf.php' );
 include_once( get_stylesheet_directory() . '/acf/advanced-custom-fields-pro/acf.php' );
 
-//	function my_register_fields()
-//	{
-//		include_once(get_template_directory().'/acf/acf-image-crop-add-on/acf-image-crop.php');
-//	}
-//	add_action('acf/register_fields', 'my_register_fields');
-
-//include (get_stylesheet_directory() . '/acf/acf-fields.php');
+include_once(get_stylesheet_directory().'/acf/advanced-custom-fields-font-awesome-customized/acf-font-awesome.php');
+include_once(get_stylesheet_directory() . '/acf/acf-image-crop-add-on-customized/acf-image-crop.php');
+//echo "this:".plugin_dir_url( __FILE__ );
+if( function_exists('acf_add_options_page') ) {
+	acf_add_options_page(array(
+		'page_title' 	=> 'Home Page',
+		'menu_slug' 	=> 'home-page-options',
+		'icon_url' 		=> 'dashicons-admin-home',
+		'redirect'		=> true,
+		'position'		=> "3.01"
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Hero',
+		'menu_title' 	=> 'Hero',
+		'parent_slug'	=> 'home-page-options',
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Services',
+		'menu_title' 	=> 'Services',
+		'parent_slug'	=> 'home-page-options',
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Portfolio',
+		'menu_title' 	=> 'Portfolio',
+		'parent_slug'	=> 'home-page-options',
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Timeline',
+		'menu_title' 	=> 'Timeline',
+		'parent_slug'	=> 'home-page-options',
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Team',
+		'menu_title' 	=> 'Team',
+		'parent_slug'	=> 'home-page-options',
+	));
+	acf_add_options_sub_page(array(
+		'page_title' 	=> 'Contact',
+		'menu_title' 	=> 'Contact',
+		'parent_slug'	=> 'home-page-options',
+	));
+	acf_add_options_page(array(
+		'page_title' 	=> 'Site Settings',
+		'menu_slug' 	=> 'site-general-settings',
+		'capability'	=> 'edit_posts',
+		'icon_url'		=> 'dashicons-admin-customizer',
+		'redirect'		=> false,
+		'position'		=> "3.02"
+	));
+}
