7ee0fe4437c3174089b19a3d8a203585a2b3c024
Dan Green <dangreenisrael@gmail.com>
2016-05-01 18:27:28 +1200

    Mobile CSS work
    Menu tweaks + backdrop
    Misc improvements

--- static/js/agency.js
+++ static/js/agency.js
@@ -4,34 +4,49 @@
  * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap/blob/gh-pages/LICENSE)
  */
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-jQuery(function() {
-    jQuery('a.page-scroll').bind('click', function(event) {
-        var jQueryanchor = jQuery(this);
-        jQuery('html, body').stop().animate({
-            scrollTop: jQuery(jQueryanchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+jQuery(document).ready(function () {
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    jQuery(function() {
+        jQuery('a.page-scroll').bind('click', function(event) {
+            var jQueryanchor = jQuery(this);
+            jQuery('html, body').stop().animate({
+                scrollTop: jQuery(jQueryanchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
     });
-});
+    var body = jQuery('body');
 
 // Highlight the top nav as scrolling occurs
-jQuery('body').scrollspy({
-    target: '.navbar-fixed-top'
-});
+    body.scrollspy({
+        target: '.navbar-fixed-top'
+    });
 
 // Closes the Responsive Menu on Menu Item Click
-var navBox = jQuery('#nav-main').find('.navbar-mobilized');
-var menuToggle = jQuery('#toggleMenu');
-var hideOnContent = function () {
-  jQuery('#content').one('click', function () {
-      navBox.removeClass('show');
-  })
-};
-menuToggle.on('click',function() {
-    navBox.toggleClass('show');
-    hideOnContent();
-});
-navBox.on('click', function (){
-    navBox.removeClass('show');
+    var navBox = jQuery('#nav-main').find('.navbar-mobilized');
+    var menuToggle = jQuery('#toggleMenu');
+    var backdrop = jQuery('#backdrop');
+    var showingMenu = false;
+    var hideOnContent = function () {
+        jQuery('#content').one('click', function () {
+            navBox.removeClass('show');
+            backdrop.modal('hide');
+        })
+    };
+    menuToggle.on('click',function() {
+        if(!showingMenu){
+            showingMenu = true;
+            navBox.addClass('show');
+            backdrop.modal('show');
+            hideOnContent();
+        } else{
+            showingMenu = false;
+            navBox.removeClass('show');
+            backdrop.modal('hide');
+        }
+    });
+    navBox.on('click', function (){
+        navBox.removeClass('show');
+        backdrop.modal('hide');
+    });
 });
