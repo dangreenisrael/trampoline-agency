c9c92fc72c7f18d6a43626533934e1c6b334ab82
Dan Green <dangreenisrael@gmail.com>
2016-07-06 18:10:25 -0400

    Added blurb area

--- acf/fields.php
+++ acf/fields.php
@@ -26,7 +26,7 @@ function my_acf_settings_dir( $dir ) {
 }
 
 // 3. Hide ACF field group menu item
-//add_filter('acf/settings/show_admin', '__return_false');
+add_filter('acf/settings/show_admin', '__return_false');
 
 // 4. Include ACF
 include_once(get_stylesheet_directory() . '/acf/advanced-custom-fields-pro/acf.php' );
