1245143cb9804b00ceb694be5830c840595790ca
Dan Green <dangreenisrael@gmail.com>
2016-07-13 16:34:08 -0400

    Minor SEO changes

--- author.php
+++ author.php
@@ -12,8 +12,10 @@ global $wp_query;
 
 $context = Timber\Timber::get_context();
 $context['posts'] = Timber\Timber::get_posts();
+wp_enqueue_style( 'global', get_stylesheet_directory_uri() . '/static/less/global.less' );
+
 if ( isset( $wp_query->query_vars['author'] ) ) {
-	$author = new TimberUser( $wp_query->query_vars['author'] );
+	$author = new Timber\User( $wp_query->query_vars['author'] );
 	$context['author'] = $author;
 	$context['title'] = 'Author Archives: ' . $author->name();
 }
